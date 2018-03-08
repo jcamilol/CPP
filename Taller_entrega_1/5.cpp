e6f92595296e1cf53191bb25078f28d52b0425b4
Juan_C_Lozano <lozanocamilo2015@gmail.com>
2018-03-07 19:39:13 -0500

    Taller para entregar 1

--- Taller_entrega_1/5.cpp
+++ Taller_entrega_1/5.cpp
@@ -1,38 +1,34 @@
 /*Sin usar el operador modulo (%), realizar un programa en C++ que pida dos números (a y b) y muestre si a es múltiplo de b .*/
 #include <iostream>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
 int main(void)
 {
     int a = 0, b = 0;
+    double c = 0;
     cout << "Ingrese dos enteros" << endl;
     cin >> a;
     cin >> b;
-
-    bool boolean = false;
-    if(a == 0){
-        boolean = true;
-    }
-    else if(a > 0){
-        for(int ii = -a; ii <= a; ii++){
-            if(a == b*ii){
-                boolean = true;
-            }
+    if(b != 0){
+        c = (((double)a)/b); //typecasting de la división a double
+        cout << "c = " << c << endl;
+        //¿Existe c entero tal que a = c*b? => a/b = c; c debe ser entero
+        if(c == floor(c)){ //Si es entero, es igual a su parte entera
+            cout << a << " SÍ es múltiplo de " << b << endl;
+        }
+        else{
+            cout << a << " NO es múltiplo de " << b << endl;
         }
     }
-    else if(a < 0){
-        for(int ii = a; ii <= -a; ii++){
-            if(a == b*ii){
-                boolean = true;
-            }
-        }
+    else if(b == 0 && a == 0){
+        cout << a << " SÍ es múltiplo de " << b << endl;
     }
-    if (boolean == true)
-        cout << a << " SÍ es múltiplo de "<< b << endl;
-    else
-        cout << a << " NO es múltiplo de "<< b << endl;
+    else if(b == 0 && a!= 0){
+        cout << a << " NO es múltiplo de " << b << endl;
+    }
 
     system("PAUSE");
     return 0;
